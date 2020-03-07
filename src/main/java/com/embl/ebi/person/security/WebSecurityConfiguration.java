a2ebf62e019a9b4bc81ce592115cece55cd38ace
Nandom PC <nandom.swipe.ng>
2020-03-07 19:41:41 +0100

    Modified WebSecurityConfiguration

--- src/main/java/com/embl/ebi/person/security/WebSecurityConfiguration.java
+++ src/main/java/com/embl/ebi/person/security/WebSecurityConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
